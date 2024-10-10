6263dec7a26a63e3f7170ef50cf5fea0ee5305a2
YoAquinjs <sebascertuche@gmail.com>
2024-10-09 18:24:44 -0600

    added filetype mapping

--- lua/config/filetype.lua
+++ lua/config/filetype.lua
@@ -10,3 +10,32 @@ for file, _ in vim.fs.dir(path) do
         end
     end
 end
+
+-- File Mapping
+
+local mappings = {
+    shebang_mapping = function()
+        local first_line = vim.fn.getline(1)
+
+        if not string.match(first_line, "^#!") then
+            return
+        end
+
+        if string.match(first_line, "bash") then
+            vim.bo.filetype = "bash"
+        elseif string.match(first_line, "sh") then
+            vim.bo.filetype = "sh"
+        elseif string.match(first_line, "python") then
+            vim.bo.filetype = "python"
+        end
+    end,
+}
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*",
+    callback = function()
+        for _, mapping in pairs(mappings) do
+            mapping()
+        end
+    end
+})
