f62bc6c3c4543c14f5484f58f5c5734dc135fa1d
YoAquinjs <sebascertuche@gmail.com>
2024-10-10 21:37:57 -0600

    refactored file mappings for priorities

--- lua/config/filetype.lua
+++ lua/config/filetype.lua
@@ -13,29 +13,80 @@ end
 
 -- File Mapping
 
+local MAX_PRIORITY = 3
+local MID_PRIORITY = 2
+local LOW_PRIORITY = 1
+local NO_PRIORITY = 0 -- No mapping should have this priority
+
 local mappings = {
-    shebang_mapping = function()
-        local first_line = vim.fn.getline(1)
+    { -- extension
+        priority = MAX_PRIORITY,
+        map = function()
+            local buf = vim.api.nvim_get_current_buf()
+            local buf_name = vim.api.nvim_buf_get_name(buf)
 
-        if not string.match(first_line, "^#!") then
-            return
-        end
+            if string.match(buf_name, ".h$") then
+                return "c"
+            elseif string.match(buf_name, ".hpp$") then
+                return "cpp"
+            end
+        end,
+    },
 
-        if string.match(first_line, "bash") then
-            vim.bo.filetype = "bash"
-        elseif string.match(first_line, "sh") then
-            vim.bo.filetype = "sh"
-        elseif string.match(first_line, "python") then
-            vim.bo.filetype = "python"
-        end
-    end,
+    { -- shebang
+        priority = LOW_PRIORITY,
+        map = function()
+            local first_line = vim.fn.getline(1)
+
+            if not string.match(first_line, "^#!") then
+                return
+            end
+
+            if string.match(first_line, "bash") then
+                return "bash"
+            elseif string.match(first_line, "sh") then
+                return "sh"
+            elseif string.match(first_line, "python") then
+                return "python"
+            end
+        end,
+    },
 }
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*",
     callback = function()
+        local filetype = ""
+        local priority = NO_PRIORITY
+
         for _, mapping in pairs(mappings) do
-            mapping()
+            if mapping.priority < priority then
+                goto continue
+            end
+
+            local mapped = mapping.map()
+            -- print("mapped", mapped)
+            if mapped == nil then
+                goto continue
+            end
+
+            if mapping.priority > priority then
+                priority = mapping.priority
+                filetype = mapped
+            elseif mapping.priority == priority then
+                if filetype == mapped then
+                    goto continue
+                end
+
+                error("filetype determination conflict")
+                error("incompatible " .. filetype " and " .. mapped)
+                return
+            end
+            ::continue::
+        end
+
+        if priority ~= NO_PRIORITY then
+            vim.bo.filetype = filetype
         end
     end
 })
