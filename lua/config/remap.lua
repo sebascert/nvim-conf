99860b6c318824023fd46ce7fbb734cac896a86f
YoAquinjs <sebascertuche@gmail.com>
2024-10-24 10:16:45 -0600

    mod remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,44 +1,54 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local keymap = vim.keymap.set
+local cmd = vim.api.nvim_create_user_command
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzz")
+keymap("n", "N", "Nzz")
 
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap("x", "<leader>p", [["_dP]])
+keymap({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
+keymap("n", "<leader>Y", [["+Y]])
+keymap({ "n", "v" }, "<leader>y", [["+y]])
 
-vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+keymap("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>S", [[:%s/<C-r>"/<C-r>"/gI<Left><Left><Left>]])
+keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap("n", "<leader>S", [[:%s/<C-r>"/<C-r>"/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
-vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
-vim.keymap.set("n", "<leader>bls", vim.cmd.ls)
-vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+keymap("n", "<leader>D",
+    "<cmd>lua vim.diagnostic.open_float(nil, { focusable = false })<CR>",
+    { noremap = true, silent = true })
 
-vim.keymap.set("n", "<C-PageUp>", vim.cmd.bn)
-vim.keymap.set("n", "<C-PageDown>", vim.cmd.bp)
+-- keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
+keymap("n", "<leader>bn", vim.cmd.bn)
+keymap("n", "<leader>bp", vim.cmd.bp)
+keymap("n", "<leader>bls", vim.cmd.ls)
+keymap("n", "<leader>bd", vim.cmd.bd)
+
+keymap("n", "<C-PageUp>", vim.cmd.bn)
+keymap("n", "<C-PageDown>", vim.cmd.bp)
+
+keymap("n", "<leader><leader>", vim.cmd.so)
+
+cmd("Bcppath", function()
+    vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, {})
