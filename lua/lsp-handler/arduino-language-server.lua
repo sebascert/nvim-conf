0576205b568f1781325bb1afd5e2e60507425a76
YoAquinjs <sebascertuche@gmail.com>
2024-11-21 05:02:48 -0600

    disable clangd when arduino-ls attaches on c and cpp files

--- lua/lsp-handler/arduino-language-server.lua
+++ lua/lsp-handler/arduino-language-server.lua
@@ -1,30 +1,42 @@
-local function get_sketch_fqbn(root_dir)
-    local fqbn_value = nil
-
-    local sketch_config = "sketch.yaml"
+local function get_sketch(root_dir)
+    local sketch_config_filename = "sketch.yaml"
     local path_sep = package.config:sub(1, 1)
 
     local current_dir = root_dir
     while current_dir do
-        local sketch_path = current_dir .. path_sep .. sketch_config
+        local sketch_path = current_dir .. path_sep .. sketch_config_filename
         local file = io.open(sketch_path, "r")
 
         if file then
-            for line in file:lines() do
-                fqbn_value = line:match("^default_fqbn:%s*(.+)")
-                if fqbn_value then
-                    break
-                end
-            end
             file:close()
-            break -- if found sketch config stop searching
+            return sketch_path
         else
             -- Move up one directory
             local parent_dir = current_dir:match("^(.*)" .. path_sep)
-            if parent_dir == current_dir then break end
+            if parent_dir == current_dir then
+                return nil
+            end
             current_dir = parent_dir
         end
     end
+end
+
+local function get_sketch_fqbn(sketch_path)
+    local file = io.open(sketch_path, "r")
+
+    if not file then
+        return nil
+    end
+
+    local fqbn_value = nil
+    for line in file:lines() do
+        fqbn_value = line:match("^default_fqbn:%s*(.+)")
+        if fqbn_value then
+            break
+        end
+    end
+
+    file:close()
 
     return fqbn_value
 end
@@ -37,7 +49,7 @@ local function on_new_config(config, root_dir)
     local arduino_cli_config = vim.fn.system("arduino-cli config get directories.data")
     arduino_cli_config = arduino_cli_config:gsub("%s+$", "") .. "/arduino_cli.yaml"
 
-    local fqbn = get_sketch_fqbn(root_dir) or "arduino:avr:uno"
+    local fqbn = get_sketch_fqbn(get_sketch(root_dir)) or "arduino:avr:uno"
 
     if not fqbn then
         return
@@ -45,19 +57,36 @@ local function on_new_config(config, root_dir)
 
     config.cmd = {
         "arduino-language-server",
-        "-clangd", clangd,
-        "-cli", arduino_cli,
-        "-cli-config", arduino_cli_config,
-        "-fqbn", fqbn,
+        "-clangd",
+        clangd,
+        "-cli",
+        arduino_cli,
+        "-cli-config",
+        arduino_cli_config,
+        "-fqbn",
+        fqbn,
     }
+end
+
+local function on_attach(client, bufnr)
+    client.capabilities.textDocument.semanticTokens = false
+    client.capabilities.workspace.semanticTokens = false
+
+    -- detach clangd
+    for _, active_client in pairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+        if active_client.name == "clangd" then
+            vim.defer_fn(function()
+                vim.lsp.buf_detach_client(bufnr, active_client.id)
+            end, 10)
+            break
+        end
+    end
 end
 
 return function()
     require("lspconfig").arduino_language_server.setup({
-        on_attach = function(client)
-            client.capabilities.textDocument.semanticTokens = false
-            client.capabilities.workspace.semanticTokens = false
-        end,
+        filetypes = { "arduino", "cpp", "h", "c" },
+        on_attach = on_attach,
         on_new_config = on_new_config,
     })
 end
--- lua/lsp-handler/clangd.lua
+++ lua/lsp-handler/clangd.lua
@@ -1,10 +1,17 @@
+local function on_attach(client, bufnr)
+    -- detach clangd on attached arduino-ls
+    for _, active_client in pairs(vim.lsp.get_clients({ bufnr = bufnr })) do
+        if active_client.name == "arduino_language_server" then
+            vim.defer_fn(function()
+                vim.lsp.buf_detach_client(bufnr, client.id)
+            end, 10)
+            break
+        end
+    end
+end
+
 return function()
-    require("lspconfig").clangd.setup({})
-    -- local lspconfig = require("lspconfig")
-    -- local config = lspconfig.clangd.document_config
-    --
-    -- config.filetypes = config.filetypes or {}
-    -- table.insert(config.filetypes, "arduino")
-    --
-    -- lspconfig.clangd.setup(config)
+    require("lspconfig").clangd.setup({
+        on_attach = on_attach,
+    })
 end
